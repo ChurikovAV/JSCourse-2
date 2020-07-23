92b8e38b1baa8f9a843fff444813c8b15aa0e539
Unknown <anatoliy.churikov@gmail.com>
2020-07-23 21:07:11 +0700

    51-52 Lessons

--- 38. Lesson - Tabs for the NewProject/js/script.js
+++ 38. Lesson - Tabs for the NewProject/js/script.js
@@ -163,9 +163,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
         render() {
             const element = document.createElement('div');
+
             if (this.classes.length === 0) {
-                this.element = 'menu__item';
-                element.classList.add(this.element);
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
             } else {
                 this.classes.forEach(className => element.classList.add(className));
             }
@@ -190,9 +191,7 @@ window.addEventListener('DOMContentLoaded', function() {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        ".menu .container",
-        'menu__item'
-        
+        ".menu .container"
     ).render();
 
     new MenuCard(
@@ -201,8 +200,7 @@ window.addEventListener('DOMContentLoaded', function() {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         14,
-        ".menu .container",
-        'menu__item'
+        ".menu .container"
     ).render();
 
     new MenuCard(
@@ -211,8 +209,7 @@ window.addEventListener('DOMContentLoaded', function() {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         21,
-        ".menu .container",
-        'menu__item'
+        ".menu .container"
     ).render();
 
 });
